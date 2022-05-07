42ac34785fec0400df155f7e074e77b69363b73c
rzhvn1 <erzhanmuratov99@gmail.com>
2022-05-07 21:50:05 +0600

    add modelviewset for Project and Task

---
+++ source/api/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminUserOrReadOnly
+from .models import Project, Task
+from .serializers import ProjectSerializer, TaskSerializer
+
+
+class ProjectModelViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = (permissions.IsAdminUser,)
+    filter_backends = [SearchFilter]
+
+
+class TaskModelViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [SearchFilter]
+
+
+
