8e149a2025b1b023c19d95858d76fddd6c8fe69b
rzhvn1 <erzhanmuratov99@gmail.com>
2022-05-07 22:39:31 +0600

    add Comment router

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectModelViewSet, TaskModelViewSet
+from .views import ProjectModelViewSet, TaskModelViewSet, CommentModelViewSet
 
 
 router = DefaultRouter()
 router.register(r'^', ProjectModelViewSet, basename='project')
 router.register(r'^task', TaskModelViewSet, basename='task')
+router.register(r'^comment', CommentModelViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
