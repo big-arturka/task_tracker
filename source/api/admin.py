d8ef9e33ee3f3733105d78ce396320e0435e86f7
rzhvn1 <erzhanmuratov99@gmail.com>
2022-05-07 22:39:05 +0600

    register Comment model

--- source/api/admin.py
+++ source/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project,Task
+from .models import Project,Task, Comment
 # Register your models here.
 
-admin.site.register([Project, Task])
+admin.site.register([Project, Task, Comment])
